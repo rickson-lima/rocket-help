10ac858c9c08782d421ecd592ec2eadfc9a8ebc5
Rickson Lima <rickson.lima.dev@gmail.com>
2022-07-18 18:43:48 -0400

    #1 - initial configs and SignIn screen interface

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { Heading, Icon, VStack, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native'
+
+import Logo from '../assets/logo_primary.svg'
+
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+
+export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { colors } = useTheme()
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" mt={20} mb={6} fontSize='xl'>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        mb={4}
+        placeholder='Email'
+        onChangeText={setEmail}
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+      />
+      <Input
+        mb={8}
+        placeholder='Senha'
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <Button title='Entrar' w='full' />
+    </VStack>
+  );
+}
